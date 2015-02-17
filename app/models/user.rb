326dbb73bef523c292913480696011994f913cc0
Sophia Kc <sophiakc@gmail.com>
2015-02-17 11:41:41 +0100

    sign in sign up done

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   has_many :flats
 
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
-  validates :phone_number, presence: true, uniqueness: true, format: { with: /\d{10}/ }, numericality: { only_interger: true }
+  # validates :name, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
+  # validates :phone_number, presence: true, uniqueness: true, format: { with: /\d{10}/ }, numericality: { only_interger: true }
 end
