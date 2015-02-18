8e7122cfb82fd1a88437c87f15033195893cbc58
Jean Hamant <jhamant@gmail.com>
2015-02-18 16:46:55 +0100

    working on homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   # validates :name, presence: true, uniqueness: true
   # validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
   # validates :phone_number, presence: true, uniqueness: true, format: { with: /\d{10}/ }, numericality: { only_interger: true }
-<<<<<<< HEAD
+
   def self.find_for_facebook_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
@@ -29,11 +29,11 @@ class User < ActiveRecord::Base
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
     end
-=======
+  end
+
   private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver
->>>>>>> 43820f1f4f459bfd92d6b4cdf30fa17e5459e4ab
   end
 end
