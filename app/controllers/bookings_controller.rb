fcc660a1022ef51c6b9b3aaaec46d5f5275a56fa
Sophia Kc <sophiakc@gmail.com>
2015-02-20 13:39:49 +0100

    changes before lunch still need to unable booking on my flats

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,14 +14,52 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
-  def create
+  # def create
+  #   @booking = Booking.new(booking_params)
+  #   @booking.flat = @flat
+  #   @booking.user = current_user
+  #   if @flat.right_dates?(@booking)
+  #     if @flat.available?(@booking)
+  #       if @booking.save
+  #         redirect_to flat_booking_path(@flat, @booking)
+  #       else
+  #         flash.now[:alert] = "Unable to create a booking request"
+  #         render :new
+  #       end
+  #     else
+  #       flash.now[:alert] = "Unavailable - please try other dates"
+  #       render :new
+  #     end
+  #   else
+  #     flash.now[:alert] = "Dates are not valid - either are before today or checkout is before checkin"
+  #     render :new
+  #   end
+  # end
+
+
+    def create
     @booking = Booking.new(booking_params)
     @booking.flat = @flat
-    @booking.user = current_user
-    if @booking.save
-      redirect_to flat_booking_path(@flat, @booking)
+    @booking.user = @current_user
+    if @flat.user != @booking_user
+      if @flat.right_dates?(@booking)
+        if @flat.available?(@booking)
+          if @booking.save
+            redirect_to flat_booking_path(@flat, @booking)
+          else
+            flash.now[:alert] = "Unable to create a booking request"
+            render :new
+          end
+        else
+          flash.now[:alert] = "Unavailable - please try other dates"
+          render :new
+        end
+      else
+        flash.now[:alert] = "Dates are not valid - either are before today or checkout is before checkin"
+        render :new
+      end
     else
-      flash.now[:alert] = "Unable to create a booking request"
+      flash.now[:alert] = "You can't book your own wagon"
       render :new
     end
   end
@@ -30,14 +68,14 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def update
-    @booking = Booking.find(params[:booking][:id])
-    if @booking.update(booking_params)
-      redirect_to flats_path
-    else
-      redirect_to profile_show_path
-    end
-  end
+  # def update
+  #   @booking = Booking.find(params[:booking][:id])
+  #   if @booking.update(booking_params)
+  #     redirect_to flats_path
+  #   else
+  #     redirect_to profile_show_path
+  #   end
+  # end
 
   private
 
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -19,4 +19,39 @@ class Flat < ActiveRecord::Base
     where("city iLIKE ?", "%#{search}%")
   end
 
+  def available?(b)
+    output = true
+    bookings.select { |bkg| bkg.status || bkg.status = nil }.each do |booking|
+      if booking.checkin <= b.checkout && booking.checkout >= b.checkin
+        output = false
+      end
+    end
+    output
+  end
+
+  def right_dates?(b)
+    output = true
+    # bookings.select { |bkg| bkg.status }.each do |booking|
+      if b.checkin < Date.today || b.checkin >= b.checkout
+        output = false
+      end
+    # end
+    output
+  end
+
+  # def pending_request?(b)
+  #   output = true
+  #   bookings.select { |bkg| bkg.status }.each do |booking|
+  #     # si un booking a été réalisé pr une date et un appart donné
+  #     # et si ce booking n'a pas encore été validé par le propriétaire
+  #     # donc que son statut n'est pas encore passé de nil>true ou de nil>false>true
+  #     # ou si son statut est déjà à true
+  #     # alors je ne peux pas le booker => message d'erreur
+
+  #     if booking.status = true || if booking.status =  <= b.checkout && booking.checkout >= b.checkin
+  #       output = false
+  #     end
+  #   end
+  #   output
+  # end
 end
