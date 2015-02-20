637ee07c73860d2d5cda0866a28840827c90f353
Sophia Kc <sophiakc@gmail.com>
2015-02-20 15:16:41 +0100

    booking date are OK

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,19 @@ class Booking < ActiveRecord::Base
   validates :checkout, presence: true
   validates :user_id, presence: true #, uniqueness: true
   validates :flat_id, presence: true #, uniqueness: true
+  validate :check_dates
+  validate :checkin_ok
+
+  def check_dates
+    if checkin > checkout
+      errors.add :checkout, "Checkout date should be after Checkin"
+    end
+  end
+
+  def checkin_ok
+    if checkin < Date.today
+      errors.add :checkin, "Checkin date should be after today"
+    end
+  end
+
 end
